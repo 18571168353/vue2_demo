9d41d768a5d0793cd6f42c891d4941252dc3fe1e
18571168353 <2730747608@qq.com>
2022-05-09 14:50:03 +0800

    添加导出pdf功能

--- src/views/emp/EmpAdv.vue
+++ src/views/emp/EmpAdv.vue
@@ -1,9 +1,10 @@
 <template>
-	<div style="margin-top:100px">
-		父组件=>子组件传过来的值:{{sonmsg}}
-		<Son :famsg="famsg" @getsonmsg="getsonmsg" style="margin-bottom:50px"/>
-		<BrotherOne/>
-		<BrotherTwo/>
+	<div class="main" style="margin-top: 100px">
+		父组件=>子组件传过来的值:{{ sonmsg }}
+		<Son :famsg="famsg" @getsonmsg="getsonmsg" style="margin-bottom: 50px" />
+		<BrotherOne />
+		<BrotherTwo />
+		<el-button type="primary" @click="exportPdf">导出</el-button>
 	</div>
 </template>
 
@@ -11,13 +12,14 @@
 import Son from './son.vue'
 import BrotherOne from './brotherone.vue'
 import BrotherTwo from './brothertwo.vue'
+import utils from '../../utils/exportPDF'
 export default {
 	name: '',
-	components: {Son,BrotherOne,BrotherTwo},
+	components: { Son, BrotherOne, BrotherTwo },
 	props: {},
 	data() {
 		return {
-			sonmsg:'',
+			sonmsg: '',
 			famsg: '我是父组件的内容'
 		}
 	},
@@ -26,8 +28,11 @@ export default {
 	created() {},
 	mounted() {},
 	methods: {
-		getsonmsg(msg){
-			this.sonmsg=msg
+		getsonmsg(msg) {
+			this.sonmsg = msg
+		},
+		exportPdf() {
+			utils.downloadPDF(document.querySelector('.main'), '自定义导出名称')
 		}
 	}
 }
